00415ac54d4921ba28c876494c148a6fde4027ed
eaydgdu <emre.aydogdu@outlook.com>
2023-05-11 23:31:44 +0200

    checked malloc
    is working

--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -6,25 +6,25 @@
 /*   By: emaydogd <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 00:26:27 by emaydogd          #+#    #+#             */
-/*   Updated: 2023/05/08 16:18:44 by emaydogd         ###   ########.fr       */
+/*   Updated: 2023/05/11 23:29:23 by emaydogd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
-char	*ft_strdup(const char *s)
+char	*ft_strdup(const char *s1)
 {
 	int		i;
 	char	*dup;
 
-	dup = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
+	dup = (char *)malloc(sizeof(char) * (ft_strlen(s1) + 1));
 	i = 0;
-	if (dup == (void *)0)
-		return ((void *)0);
+	if (dup == NULL)
+		return (NULL);
 	else
 	{
-		while (s[i])
+		while (s1[i])
 		{
-			dup[i] = s[i];
+			dup[i] = s1[i];
 			i++;
 		}
 		dup[i] = '\0';

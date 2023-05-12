6e97d06edbe5e527b2835d497b00cbf4ab8364f2
eaydgdu <emre.aydogdu@outlook.com>
2023-05-12 13:06:28 +0200

    fix substr

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,27 +6,24 @@
 /*   By: emaydogd <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 00:26:27 by emaydogd          #+#    #+#             */
-/*   Updated: 2023/05/10 14:04:57 by emaydogd         ###   ########.fr       */
+/*   Updated: 2023/05/11 23:37:55 by emaydogd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char	*sub;
-	size_t	s_len;
+	char	*ret;
 
-	if (s == NULL)
+	if (!s)
 		return (NULL);
-	s_len = ft_strlen(s);
-	if (s_len <= len)
-		len = s_len - start;
-	if (start > s_len)
-		return (ft_strdup(""));
-	sub = ft_calloc(len + 1, sizeof(char));
-	if (sub == NULL)
+	if (ft_strlen(s) < start)
+		len = 0;
+	if (ft_strlen(s + start) < len)
+		len = ft_strlen(s) - start;
+	ret = ft_calloc(len + 1, sizeof(char));
+	if (ret == NULL)
 		return (NULL);
-	else
-		ft_strlcpy(sub, s + start, len + 1);
-	return (sub);
+	ft_strlcpy(ret, s + start, len + 1);
+	return (ret);
 }

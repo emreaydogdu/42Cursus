8703662ff254fbb78a5b387357876a1bfb781f09
emreaydogdu <emre.aydogdu@outlook.de>
2024-05-09 20:52:58 +0200

    init philo

--- philosophers/ft_philosophers.c
+++ philosophers/ft_philosophers.c
@@ -18,7 +18,7 @@ long	getTime()
 {
 	struct timeval time;
 	gettimeofday(&time, NULL);
-	return(time.tv_usec * 1000);
+	return(time.tv_sec * 1000 + time.tv_usec/1000);
 }
 
 void *eat(void *p)
@@ -33,43 +33,36 @@ void *eat(void *p)
 	rightFork = (philo->pos + 1) % philo->table->num_philos;
 	iseating = 0;
 
-	while( philo->num_must_eat > 0)
+	while(philo->num_must_eat > 0)
 	{
-		printf("%ld	%ld is thinking.\n", getTime(), philo->pos);
-		pthread_mutex_lock(&philo->table->lock);
-		printf("%ld	%d : %ld	%ld:%ld\n", philo->pos, philo->time_last_eat, getTime(), getTime() - philo->time_last_eat, philo->table->time_to_die);
+		printf("%ld %ld is thinking\n", getTime(), philo->pos);
 		if (getTime() - philo->time_last_eat > philo->table->time_to_die)
 		{
-			printf("%ld	%ld died\n", getTime(), philo->pos);
+			printf("%ld %ld died\n", getTime(), philo->pos);
 			pthread_mutex_unlock(&philo->table->lock);
 			break;
 		}
-		printf("%ld	%ld trys to eat\n", getTime(), philo->pos);
+		pthread_mutex_lock(&philo->table->lock);
 		if(philo->table->forks[leftFork] == 1 && philo->table->forks[rightFork] == 1){
 			philo->table->forks[leftFork]  = 0;
 			philo->table->forks[rightFork] = 0;
-			printf("%ld	%ld has taken a fork\n", getTime(), philo->pos);
+			printf("%ld %ld has taken a fork\n", getTime(), philo->pos);
 			philo->num_must_eat--;
 			iseating = 1;
 		}
-		pthread_mutex_unlock(&philo->table->lock);
-
 		if(iseating){
-			pthread_mutex_lock(&philo->table->lock);
-			printf("%ld	%ld is eating\n", getTime(), philo->pos);
+			printf("%ld %ld is eating\n", getTime(), philo->pos);
 			usleep(philo->table->time_to_eat);
 			philo->time_last_eat = getTime();
 			philo->table->forks[leftFork]  = 1;
 			philo->table->forks[rightFork] = 1;
 			iseating = 0;
-			pthread_mutex_unlock(&philo->table->lock);
-
-			if ( philo->num_must_eat == 0 )
-				printf("%ld	%ld finished eating\n", getTime(), philo->pos);
-
-			printf("%ld	%ld is sleeping\n", getTime(), philo->pos);
-			usleep(philo->table->time_to_sleep);
 		}
+		pthread_mutex_unlock(&philo->table->lock);
+		printf("%ld %ld is sleeping\n", getTime(), philo->pos);
+		usleep(philo->table->time_to_sleep);
+
+
 	}
 	pthread_exit (NULL);
 }
@@ -88,14 +81,14 @@ int main(int argc, const char * argv[])
 	for(long i = 0; i < table.num_philos; i++)
 	{
 		table.forks[i] = 1;
-		table.philos[i].pos = i;
+		table.philos[i].pos = i+1;
 		table.philos[i].table = &table;
 		table.philos[i].time_last_eat = getTime();
 		table.philos[i].num_must_eat = atoi(argv[5]);
 	}
 	for (long t = 0; t < table.num_philos; t++)
 	{
-		int rc = pthread_create(&table.philos[t].thread, NULL, eat, &table.philos[t]);
+		int rc = pthread_create(&table.philos[t].thread, NULL, &eat, &table.philos[t]);
 		if (rc){
 			printf("ERROR; return code from pthread_create () is %d\n", rc);
 			exit(-1);
